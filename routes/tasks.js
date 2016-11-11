213d2faad14be004a4dfa88d541372a3d494e241
benjaminsw <benjaminsw@live.com>
2016-11-11 22:15:19 +0000

    updated tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,1 +1,9 @@
-// JavaScript source code
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/tasks', function (req, res, next) {
+    res.send('TASK API');
+});
+
+module.exports = router;
