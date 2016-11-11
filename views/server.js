09c715b75efa9a47a7bb754b81e97297f2de39d5
benjaminsw <benjaminsw@live.com>
2016-11-11 22:19:29 +0000

    updated server.js

---
+++ views/server.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./rotes/index');
+var task = require('./rotes/task');
+
+var port = 3000;
+
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// set static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(port, function () {
+    console.log('Server started on port ' + port);
+});
