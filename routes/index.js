ee03fde49c7a0223ab97739cfa6d5fffd8877454
benjaminsw <benjaminsw@live.com>
2016-11-11 22:14:16 +0000

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
 // JavaScript source code
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.send('INDEX PAGE');
+});
+
+module.exports = router;
