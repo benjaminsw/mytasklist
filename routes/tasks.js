a59a4f9072554a26d1f566aa7edde9551b169dda
benjaminsw <benjaminsw@live.com>
2016-11-12 20:25:31 +0000

    added database connection var

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://bensw:Rayong21120@ds151707.mlab.com:51707/mytasklist_db', ['tasks'])
 
 router.get('/tasks', function (req, res, next) {
-    res.render('index.html');
+    //res.render('index.html');
+    
 });
 
 module.exports = router;
