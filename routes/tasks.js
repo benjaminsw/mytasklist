8510050f3324f89cc1a3eda7169d2f35a94a20fe
benjaminsw <benjaminsw@live.com>
2016-11-12 20:27:08 +0000

    tried to make a query from db

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,7 +5,12 @@ var db = mongojs('mongodb://bensw:Rayong21120@ds151707.mlab.com:51707/mytasklist
 
 router.get('/tasks', function (req, res, next) {
     //res.render('index.html');
-    
+    db.task.find(function (err, tasks) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
