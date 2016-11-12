f535338c1faecd9589c3caaaf826a7a3993ef751
benjaminsw <benjaminsw@live.com>
2016-11-12 20:06:02 +0000

    fixed typed error in task.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/tasks', function (req, res, next) {
-    res.send('TASK API');
+    res.render('index.html');
 });
 
 module.exports = router;
