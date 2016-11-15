e964070544f5894c6b258685e128ee310b7f9a92
benjaminsw <benjaminsw@live.com>
2016-11-15 18:31:43 +0000

    updated app.module.ts

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,1 +1,7 @@
-﻿
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [BrowserModule]
+})
+export class AppModule { }
