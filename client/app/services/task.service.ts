080fd47cc5242953f9e414f8d4ba82542c2fc4fc
benjaminsw <benjaminsw@live.com>
2016-11-20 22:21:15 +0000

    updated deleteTask(id)

--- client/app/services/task.service.ts
+++ client/app/services/task.service.ts
@@ -8,14 +8,19 @@ export class TaskService {
         console.log('Task Service Initialised ...');
     }
     getTask() {
-        return this.http.get('http://localhost:3000/api/tasks')
+        return this.http.get('api/tasks')
             .map(res => res.json());
     }
     addTask(newTask) {
-        var header = new Headers();
+        var headers = new Headers();
         headers.append('Content-Type', 'applicatio/json');
-        return this.http.post('http://localhost:3000/api/task', JSON.stringify(newTask), { headers: headers });
+        return this.http.post('/api/task', JSON.stringify(newTask), { headers: headers });
         .map(res => res.json());
 
+    }
+
+    deleteTask(id) {
+        return this.http.delete('/api/task/' + id)
+            .map(res => res.json());
     }
 }
